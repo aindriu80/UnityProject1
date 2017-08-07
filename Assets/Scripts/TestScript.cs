436ce051bf4577a9c7888b51c228d130c4d5adac
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-07 11:20:33 +0100

    Countdown timer

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -3,22 +3,32 @@ using UnityEngine.UI;
 
 
 public class TestScript : MonoBehaviour {
-        public string name;
         BoxCollider boxCol;
-    public Text text;
+        public Text text;
+        public float timer;
         
 
         // Use this for initialization
         void Start ()
         {
-            text.text = name;
+
+            text.text = timer.ToString();
             boxCol = GetComponent<BoxCollider>();
             boxCol.isTrigger = false;
         }
 	
         // Update is called once per frame
-        void Update () {
-            Debug.Log(name);
+        void Update ()
+        {
+            if (timer >= 0)
+            {
+                timer -= Time.deltaTime;
+                text.text = timer.ToString("#.00");
+            }
+            else
+            {
+                text.text = "0.0";
+            }
 
         }
     }
