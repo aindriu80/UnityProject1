66371f94e0636805743655050d5548a804edd42a
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-07 09:59:59 +0100

    C# events on startup adding C# code

---
+++ Assets/testscript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class testscript : MonoBehaviour
+{
+
+    // Use this for initialization
+    public string name;
+    public Text text;
+
+    void Start()
+    {
+        text.text = name;
+        //boxCol = GetComponent<BoxCollider>();
+        //boxCol.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(name);
+
+    }
+}
