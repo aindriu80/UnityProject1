39fb4c1887458b13ae564b1a7bb6e3fde6c8b246
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-07 10:56:50 +0100

    Text Script c# working

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TestScript : MonoBehaviour {
+        public string name;
+        BoxCollider boxCol;
+    public Text text;
+        
+
+        // Use this for initialization
+        void Start ()
+        {
+            text.text = name;
+            boxCol = GetComponent<BoxCollider>();
+            boxCol.isTrigger = false;
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            Debug.Log(name);
+
+        }
+    }
+
