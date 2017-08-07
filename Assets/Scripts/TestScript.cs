f91862607dfb407979b6d282cefff03263d6e2fb
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-07 11:29:55 +0100

    Create a decimal clock

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,35 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 
 public class TestScript : MonoBehaviour {
-        BoxCollider boxCol;
-        public Text text;
-        public float timer;
         
+        public Text text;
+        string hour;
+        string minute;
+        string second;
 
         // Use this for initialization
         void Start ()
         {
-
-            text.text = timer.ToString();
-            boxCol = GetComponent<BoxCollider>();
-            boxCol.isTrigger = false;
         }
 	
         // Update is called once per frame
         void Update ()
         {
-            if (timer >= 0)
-            {
-                timer -= Time.deltaTime;
-                text.text = timer.ToString("#.00");
-            }
-            else
-            {
-                text.text = "0.0";
-            }
+        CreateClock();
+        }
 
+        void CreateClock()
+        {
+            DateTime currentTime = DateTime.Now;
+            hour = currentTime.Hour.ToString();
+            minute = currentTime.Minute.ToString();
+            second = currentTime.Second.ToString();
+            text.text = hour + ":" + minute + ":" + second;
         }
     }
 
