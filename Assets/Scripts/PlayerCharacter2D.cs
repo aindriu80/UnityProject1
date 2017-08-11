75b95e5018e9910a11c7eb74cfb44a47d6cdbeb2
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-11 11:17:03 +0100

    Translation from standing to Running

--- Assets/Scripts/PlayerCharacter2D.cs
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -7,6 +7,7 @@ public class PlayerCharacter2D : MonoBehaviour
 
     public int speed;
     public SpriteRenderer sr;
+    public Animator anim;
 
 	// Use this for initialization
 	void Start () {
@@ -22,13 +23,16 @@ public class PlayerCharacter2D : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
+            anim.SetBool("isWalking", true);
             transform.Translate(new Vector3(speed*Time.deltaTime, 0,0));
             sr.flipX = true;
         }
         else if (Input.GetKey(KeyCode.A))
         {
+            anim.SetBool("isWalking", true);
             transform.Translate(new Vector3(-speed*Time.deltaTime,0,0));
             sr.flipX = false;
         }
+        else anim.SetBool("isWalking", false);
     }
 }
