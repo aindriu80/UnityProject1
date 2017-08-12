7f4aae59563afc4b1e9c37bd913c38dfdfdbfb3d
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-12 10:31:35 +0100

    Character jumping ability

--- Assets/Scripts/PlayerCharacter2D.cs
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -1,38 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerCharacter2D : MonoBehaviour
 {
-
+    public Animator anim;
+    private bool isGrounded;
+    public float jumpForce;
+    public Rigidbody2D rig;
     public int speed;
     public SpriteRenderer sr;
-    public Animator anim;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Movement();
-	}
-
-    void Movement()
+    // Use this for initialization
+    private void Start()
     {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (isGrounded)
+            {
+                rig.AddForce(Vector2.up * jumpForce);
+                isGrounded = false;
+            }
         if (Input.GetKey(KeyCode.D))
         {
             anim.SetBool("isWalking", true);
-            transform.Translate(new Vector3(speed*Time.deltaTime, 0,0));
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             sr.flipX = true;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             anim.SetBool("isWalking", true);
-            transform.Translate(new Vector3(-speed*Time.deltaTime,0,0));
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
             sr.flipX = false;
         }
-        else anim.SetBool("isWalking", false);
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+            isGrounded = true;
     }
 }
