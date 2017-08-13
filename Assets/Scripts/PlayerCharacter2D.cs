cb0aae6f4bd97a43b27c3f0f26562efd5306f3b9
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-13 11:49:54 +0100

    Adding the shooting animation

--- Assets/Scripts/PlayerCharacter2D.cs
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -6,6 +6,8 @@ public class PlayerCharacter2D : MonoBehaviour
     private bool isGrounded;
     public float jumpForce;
     public Rigidbody2D rig;
+    private float shootTimer = .2f;
+    private bool shotTimer;
     public int speed;
     public SpriteRenderer sr;
 
@@ -13,37 +15,54 @@ public class PlayerCharacter2D : MonoBehaviour
     private void Start()
     {
         isGrounded = true;
+        shotTimer = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (shotTimer)
+            shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0)
+        {
+            shotTimer = false;
+            anim.SetBool("isShooting", false);
+            shootTimer = .2f;
+        }
+
         Movement();
-        if (isGrounded) anim.SetBool("isGrounded",true);
+        if (isGrounded) anim.SetBool("isGrounded", true);
         else anim.SetBool("isGrounded", false);
         if (rig.velocity.y < 0)
-        {
             rig.velocity += Vector2.up * Physics2D.gravity.y * 2 * Time.deltaTime;
-        }
     }
 
     private void Movement()
     {
+        if (Input.GetMouseButton(0))
+            if (!shotTimer)
+            {
+                anim.SetBool("isShooting", true);
+                shotTimer = true;
+            }
         if (Input.GetKeyDown(KeyCode.Space))
             if (isGrounded)
             {
                 rig.AddForce(Vector2.up * jumpForce);
+                anim.SetBool("isShooting", false);
                 isGrounded = false;
             }
         if (Input.GetKey(KeyCode.D))
         {
             anim.SetBool("isWalking", true);
+            anim.SetBool("isShooting", false);
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             sr.flipX = true;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             anim.SetBool("isWalking", true);
+            anim.SetBool("isShooting", false);
             transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
             sr.flipX = false;
         }
