623da6e8121d6bdad1e569e1c04af3e2dd691120
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-14 10:34:11 +0100

    Finish Up 2D Game: Particles and Polish

---
+++ Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour
+{
+    public float timer;
+    public GameObject particle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (particle)
+                Instantiate(particle, transform.position, transform.rotation);
+            Destroy(gameObject);
+
+        }
+
+    }
+}
